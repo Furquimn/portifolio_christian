ff0ccd027b204b8bd112785068e1e3a2159bde0d
Christian Furquim <christian.furquim@hotmail.com>
2025-12-15 14:11:45 -0300

    iniciando projeto

---
+++ scr/components/HomeHero.jsx
@@ -0,0 +1,9 @@
+export default function HomeHero() {
+  return (
+    <section className="bg-green-300">
+      <div className="container mx-auto px-6 bg-green-900">
+        <div className="">teste</div>
+      </div>
+    </section>
+  )
+}
---
+++ scr/components/HomeNavBar.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function HomeNavBar() {
+  return (
+    <div className="p-4 h-auto w-auto bg-amber-800">
+      <h1 className="text-2xl">Christian Furquim</h1>
+    </div>
+  );
+}
